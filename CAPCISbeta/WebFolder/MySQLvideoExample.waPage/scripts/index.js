4d5f42a84e21adcd3c2b0c98e5e4e8a432745ffc
CapCis <talevey@courtap.com>
2014-07-20 01:05:01 -0500

    Update #18

---
+++ CAPCISbeta/WebFolder/MySQLvideoExample.waPage/scripts/index.js
@@ -0,0 +1,28 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+	var button1 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		// Add your code here
+	};// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		//debugger;
+		mySakilaSource = rpcMySQLvideoExample.getSakilaActorTable();
+		sources.mySakilaSource.sync();
+		//sources.table1.all();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+	WAF.addListener("button1", "click", button1.click, "WAF");
+// @endregion
+};// @endlock
---
+++ CAPCISbeta/WebFolder/testCoding.waPage/scripts/index.js
@@ -0,0 +1,21 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		//debugger;
+		ds.Table1.method1();
+		sources.table1.all();
+		
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
+// @endregion
+};// @endlock
