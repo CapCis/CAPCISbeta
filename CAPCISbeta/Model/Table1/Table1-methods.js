36e30a7607760dd29ae097cf6149c8775f4ef167
CapCis <talevey@courtap.com>
2014-07-20 01:07:17 -0500

    workingMYSQLgrids

---
+++ CAPCISbeta/Model/Table1/Table1-methods.js
@@ -0,0 +1,28 @@
+﻿
+
+model.Table1.methods.method1 = function() {
+	//debugger;
+	var dbconnect = require('waf-mysql');
+	var param ={
+		hostname: 'localhost',
+		user: 'root',
+		password: '535CapCis',
+		database: 'sakila',
+		port: 3306,
+		ssl: false		
+};
+	var connection = dbconnect.connect(param);
+	//if (connection.isconnected)
+		{
+			var result = connection.select("first_name,last_name", "actor");
+			while (result.hasNext())
+			{
+				var row = result.getNextRow();
+				var student = ds.Table1.createEntity();
+				student.firstname = row.first_name;
+				student.lastname = row.last_name;
+				student.save();				
+			};
+		};
+};
+model.Table1.methods.method1.scope = "public";
