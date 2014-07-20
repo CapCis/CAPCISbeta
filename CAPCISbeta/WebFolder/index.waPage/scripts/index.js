b08fa781eb43679df7b06543e8738e3b0105ea64
CapCis <talevey@courtap.com>
2014-07-20 01:03:47 -0500

    Update #15

--- CAPCISbeta/WebFolder/index.waPage/scripts/index.js
+++ CAPCISbeta/WebFolder/index.waPage/scripts/index.js
@@ -3,7 +3,6 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
 	var documentEvent = {};	// @document
-	var button1 = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
@@ -13,19 +12,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		// Add your code here
 	};// @lock
 
-	button1.dblclick = function button1_dblclick (event)// @startlock
-	{// @endlock
-		ope
-	};// @lock
-
-	button1.click = function button1_click (event)// @startlock
-	{// @endlock
-	  document.getElementById("button1").innerHTML = "Hello"
-	};// @lock
-
 // @region eventManager// @startlock
-	WAF.addListener("button1", "dblclick", button1.dblclick, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
-	WAF.addListener("button1", "click", button1.click, "WAF");
 // @endregion
 };// @endlock
