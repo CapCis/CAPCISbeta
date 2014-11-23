b4293515e337b06cfd59856543be5b4fa0a01cf1
CapCis <talevey@courtap.com>
2014-11-22 19:31:01 -0600

    Update #46

--- CAPCISbeta/Model/MySQLPassThrough/MySQLPassThrough-methods.js
+++ CAPCISbeta/Model/MySQLPassThrough/MySQLPassThrough-methods.js
@@ -19,6 +19,67 @@ model.MySQLPassThrough.methods.getMyData = function(fname,lname) {
 	//var result = connection.execute('SELECT * FROM test_table WHERE CONTAINS(lastname,"e")');   //Correct Syntax does not return string
 	var result = connection.execute('SELECT * FROM test_table WHERE lastname LIKE "%ev%"');
 	MyResults = result.getAllRows();
+	
+	
+	// Create a 10 second pause 
+	var sleep = 20000   //sleep timer = 10 seconds
+	var start = new Date().getTime();   //set the reference time
+	for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > sleep){
+      break;
+  	  };
+    };
+   
+    
+    
+	connection.close;
+	return MyResults;
+	//while (result.hasNext()) {
+	//	var MyResults = result.getNextRow();
+		
+	    //fname.push(MyResults.firstname);
+	    //lname.push(MyResults.lastname);
+	     	
+	//	ds.MySQLPassThrough.name = result.getNextRow();
+	//var myFirstName = result.getNextRow();
+	//var look = myFirstName.firstname
+	//	debugger;
+	
+	//return [fname, lname];      //Returns an Array that holds two arrays fname and lname
+};
+
+model.MySQLPassThrough.methods.getMyData2 = function(fname,lname) {
+	var dbconnect = require('waf-sql');
+	var param ={
+		hostname: '68.106.70.68',
+		user: 'teamcapcis2',
+		password: 'teamcapcis2',
+		database: 'test',
+		port: 3306,
+		ssl: false,
+		dbType: 'mysql'
+}
+		
+	var connection = dbconnect.connect(param);
+		
+	//var result = connection.select("firstname,lastname","test_table",{lastname: "Levey"});
+	//var result = connection.execute("SELECT * FROM test_table WHERE id_testtable1 > '0'");
+	//var result = connection.execute('SELECT * FROM test_table WHERE CONTAINS(lastname,"e")');   //Correct Syntax does not return string
+	var result = connection.execute('SELECT * FROM test_table WHERE lastname LIKE "%ev%"');
+	MyResults = result.getAllRows();
+	
+	
+	// Create a 10 second pause 
+	//var sleep = 10000   //sleep timer = 10 seconds
+	//var start = new Date().getTime();   //set the reference time
+	//for (var i = 0; i < 1e7; i++) {
+    //if ((new Date().getTime() - start) > sleep){
+    //  break;
+  	//  };
+    //};
+   
+    
+    
 	connection.close;
 	return MyResults;
 	//while (result.hasNext()) {
@@ -35,3 +96,4 @@ model.MySQLPassThrough.methods.getMyData = function(fname,lname) {
 	//return [fname, lname];      //Returns an Array that holds two arrays fname and lname
 };
 model.MySQLPassThrough.methods.getMyData.scope = "public";
+model.MySQLPassThrough.methods.getMyData2.scope = "public";
--- CAPCISbeta/WebFolder/getMySQLDataThroughNoSQL.waPage/scripts/index.js
+++ CAPCISbeta/WebFolder/getMySQLDataThroughNoSQL.waPage/scripts/index.js
@@ -2,15 +2,42 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var MultiThreaded_Test = {};	// @button
 	var button1 = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	MultiThreaded_Test.click = function MultiThreaded_Test_click (event)// @startlock
+	{// @endlock
+		ds.MySQLPassThrough.getMyData2({onSuccess: handleOnReturnDS,
+		onError: function(event){
+			alert(event.error[0].message);
+		}})
+		
+		
+	function handleOnReturnDS(event) { 				//Function to handle onSuccess
+		arr1 = event.result;
+		sources.arr1.sync(); 
+	}	
+	};// @lock
+
 	button1.click = function button1_click (event)// @startlock
 	{// @endlock
-		arr1 = ds.MySQLPassThrough.getMyData();
-		sources.arr1.sync();
+		//arr1 = ds.MySQLPassThrough.getMyData();						//synchronous call to server
+		//sources.arr1.sync();											//synchronous call to server
+		
+	ds.MySQLPassThrough.getMyData({onSuccess: handleOnReturnDS,
+		onError: function(event){
+			alert(event.error[0].message);
+		}})
+		
+		
+	function handleOnReturnDS(event) { 				//Function to handle onSuccess
+		arr1 = event.result;
+		sources.arr1.sync(); 
+	}	
+		
 		//var mydata = ds.MySQLPassThrough.getMyData();
 		//sources.mySQLPassThrough.sync();
 		
@@ -26,6 +53,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("MultiThreaded_Test", "click", MultiThreaded_Test.click, "WAF");
 	WAF.addListener("button1", "click", button1.click, "WAF");
 // @endregion
 };// @endlock
